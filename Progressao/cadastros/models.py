73612dad265a1219b570faf628121d1294edaa47
Gabriel Moya <gabrielmoya201415@gmail.com>
2020-04-14 16:11:42 -0300

    Adicionando validade no cupom.models

--- Progressao/cadastros/models.py
+++ Progressao/cadastros/models.py
@@ -104,7 +104,7 @@ class ProdutoVenda(models.Model):
 class Cupom(models.Model):
     nome = models.CharField(max_length=100)
     desconto = models.DecimalField(max_digits=3, decimal_places=0)
-    validade = models.DateTimeField(verbose_name='data de validade', null=True)
+    validade = models.DateField(verbose_name='data de validade', null=True)
 
     def __str__(self):
         return self.nome, self.desconto
